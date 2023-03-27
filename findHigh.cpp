83c59990f02c13c27e5770b22019ff606a12f824
studentmcbride62 <111594640+studentmcbride62@users.noreply.github.com>
2023-03-27 16:26:46 -0400

    Add files via upload

--- findHigh.cpp
+++ findHigh.cpp
@@ -1,2 +1,11 @@
 // Definition of findHigh function goes here
 
+void findHigh(double *data, int size, int &index, double &highVal){
+    highVal = data[0];
+    for (int i=0; i<size; i++){
+        if (data[i] > highVal){
+            highVal = data[i];
+            index = i;
+        }
+    }
+}
