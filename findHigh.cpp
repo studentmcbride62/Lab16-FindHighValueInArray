0c7938e283c2cab9ad211309dbce739a68423757
studentmcbride62 <111594640+studentmcbride62@users.noreply.github.com>
2023-04-05 12:57:29 -0400

    Update findHigh.cpp

--- findHigh.cpp
+++ findHigh.cpp
@@ -1,11 +1,5 @@
 // Definition of findHigh function goes here
 
 void findHigh(double *data, int size, int &index, double &highVal){
-    highVal = data[0];
-    for (int i=0; i<size; i++){
-        if (data[i] > highVal){
-            highVal = data[i];
-            index = i;
-        }
-    }
+ 
 }
